a271b070c2145db7404e0bacb486dcf88e2ee818
lyq <lhcmaple@gmail.com>
2021-03-07 22:29:43 +0800

    auto update

--- http_conn/http_conn.cpp
+++ http_conn/http_conn.cpp
@@ -2,8 +2,140 @@
  * 
  */
 
-enum 
+#include "http_conn.h"
 
-class http_parser{
+HTTP_STATUS http_parser::parse(int _fd)
+{
+    fd=_fd;
+    start=0;
+    line_start=0;
+    end=0;
+    http_status=REQUEST_STATUS;
+    line_status=LINE_OPEN;
 
-};
+    uri.clear();
+    version.clear();
+    headers.clear();
+
+    while(http_status!=DONE_STATUS
+        &&http_status!=ERROR_STATUS)
+    {
+        switch(http_status)
+        {
+            case REQUEST_STATUS:
+                switch(line_status)
+                {
+                    case LINE_OPEN:
+                        line_status=line_parse();
+                        break;
+                    case LINE_CLOSE:
+                        line_status=LINE_OPEN;
+                        http_status=request_parse();
+                        break;
+                }
+                break;
+            case HEADER_STATUS:
+                switch(line_status)
+                {
+                    case LINE_OPEN:
+                        line_status=line_parse();
+                        break;
+                    case LINE_CLOSE:
+                        line_status=LINE_OPEN;
+                        http_status=header_parse();
+                        break;
+                }
+                break;
+            case DATA_STATUS:
+                break;
+        }
+    }
+    return http_status;
+}
+
+LINE_STATUS http_parser::line_parse()
+{
+    if(start==end)
+    {
+        if(start==CACHE_SIZE)
+        {
+            http_status=ERROR_STATUS;
+            return LINE_ERROR;
+        }
+        int len=recv(fd,cache+end,CACHE_SIZE-end,0);
+        if(len<0)
+        {
+            http_status=ERROR_STATUS;
+            return LINE_ERROR;
+        }
+        end=end+len;
+    }
+    for(int i=start;i<end;++i)
+    {
+        if(cache[i]=='\n')
+        {
+            if(i-1>=line_start&&cache[i-1]=='\r')
+                line_end=i-1;
+            else
+                line_end=i;
+            start=i+1;
+            return LINE_CLOSE;
+        }
+    }
+    return LINE_OPEN;
+}
+
+HTTP_STATUS http_parser::request_parse()
+{
+    int index=line_start;
+
+    while(index<line_end&&cache[index]!=' ') index++;
+    if(index==line_end)
+        return ERROR_STATUS;
+    string tmp(cache+line_start,index-line_start);
+    if(tmp==string("GET"))
+        method=GET;
+    else
+        method=UNKNOWN;
+    
+    while(index<line_end&&cache[index]==' ') index++;
+    if(index==line_end)
+        return ERROR_STATUS;
+    line_start=index;
+    while(index<line_end&&cache[index]!=' ') index++;
+    if(index==line_end)
+        return ERROR_STATUS;
+    uri.assign(cache+line_start,index-line_start);
+
+    while(index<line_end&&cache[index]==' ') index++;
+    if(index==line_end)
+        return ERROR_STATUS;
+    version.assign(cache+index,line_end-index);
+
+    line_start=start;
+    return HEADER_STATUS;
+}
+
+HTTP_STATUS http_parser::header_parse()
+{
+    if(line_start==line_end)
+    {
+        line_start=start;
+        return DATA_STATUS;
+    }
+
+    header htmp;
+    int index=line_start;
+    while(index<line_end&&cache[index]!=' '&&cache[index]!=':') index++;
+    if(index==line_end)
+        return ERROR_STATUS;
+    htmp.property.assign(cache+line_start,index-line_start);
+    
+    while(index<line_end&&(cache[index]==' '||cache[index]==':')) index++;
+    if(index==line_end)
+        return ERROR_STATUS;
+    htmp.value.assign(cache+index,line_end-index);
+
+    line_start=start;
+    return HEADER_STATUS;
+}
--- http_conn/http_conn.h
+++ http_conn/http_conn.h
@@ -1,6 +1,61 @@
 #ifndef _HTTP_CONN_H
 #define _HTTP_CONN_H
 
+#include <vector>
+#include <string>
+#include <sys/socket.h>
 
+using std::string;
+using std::vector;
+
+#define CACHE_SIZE 4096
+
+enum HTTP_STATUS{
+    REQUEST_STATUS=0,
+    HEADER_STATUS=1,
+    DATA_STATUS=2,
+    DONE_STATUS=3,
+    ERROR_STATUS=4
+};
+
+enum LINE_STATUS{
+    LINE_CLOSE=0,
+    LINE_OPEN=1,
+    LINE_ERROR=2
+};
+
+enum REQUEST_TYPE{
+    GET=0,
+    UNKNOWN=1
+};
+
+struct header{
+    string property;
+    string value;
+};
+
+class http_parser{
+private:
+    char cache[CACHE_SIZE];
+    int line_start;
+    int line_end;
+    int start;
+    int end;
+    int datalen;
+    HTTP_STATUS http_status;
+    LINE_STATUS line_status;
+    int fd;
+public:
+    REQUEST_TYPE method;
+    string uri;
+    string version;
+    vector<header> headers;
+public:
+    HTTP_STATUS parse(int _fd);
+    LINE_STATUS line_parse();
+    HTTP_STATUS request_parse();
+    HTTP_STATUS header_parse();
+    HTTP_STATUS data_parse();
+};
 
 #endif
